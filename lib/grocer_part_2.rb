f045bfd08c8a35fe1550756fe80d63800d14966a
Eve Reichmann <ereichmann4@aol.com>
2020-06-17 17:54:30 +0000

    Automatically backed up by Learn

--- lib/grocer_part_2.rb
+++ lib/grocer_part_2.rb
@@ -4,6 +4,7 @@ require 'pry'
 def apply_coupons(cart, coupons)
   with_coupons = Array.new 
   count = 0
+  while count < cart.length do
   cart.each do |grocery_item|
     
   #find_item_by_name_in_collection
