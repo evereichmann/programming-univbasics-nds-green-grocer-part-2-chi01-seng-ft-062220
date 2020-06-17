e703d61e754551b002c0143194366067ad6aba35
Eve Reichmann <ereichmann4@aol.com>
2020-06-17 18:02:21 +0000

    Automatically backed up by Learn

--- lib/grocer_part_2.rb
+++ lib/grocer_part_2.rb
@@ -2,17 +2,21 @@ require_relative './part_1_solution.rb'
 
 require 'pry'
 def apply_coupons(cart, coupons)
-  with_coupons = Array.new 
   count = 0
   while count < coupons.length 
-    cart_item = find_item_by_name_in_collection()
+    cart_item = find_item_by_name_in_collection(coupons[counter][:item], cart)
+    coupon_item_name = "#{coupons[counter][:item]} W/COUPON"
+    cart_item_with_coupon = find_item_by_name_in_collection(coupon_item_name, cart)
+    if cart_item && cart_item[:count] >= coupons[count][:num] 
+      
     end 
     count += 1
   end
 end
 
+# new array
 #find_item_by_name_in_collection
-  #consolidate_cart = cart
+#consolidate_cart = cart
 
 def apply_clearance(cart)
  
